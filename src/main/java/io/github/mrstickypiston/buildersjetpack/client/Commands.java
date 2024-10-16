7299ff8bb201ecb53f81b82385a0760720c5ef84
sticky <mr.sticky.piston@gmail.com>
2024-10-16 17:39:10 +0200

    version 1.4.7, fixed sinytra connector crashing (#6)

--- src/main/java/io/github/mrstickypiston/buildersjetpack/client/Commands.java
+++ src/main/java/io/github/mrstickypiston/buildersjetpack/client/Commands.java
@@ -2,11 +2,10 @@ package io.github.mrstickypiston.buildersjetpack.client;
 
 import io.github.mrstickypiston.buildersjetpack.client.commands.BuildersJetpackCommand;
 
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 
 public class Commands {
     public static void registerCommands() {
-
-        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess) -> BuildersJetpackCommand.register(dispatcher));
+        CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, registrationEnvironment) -> BuildersJetpackCommand.register(dispatcher));
     }
 }
